47510a86f66f6a6773ac4b2a94979b220ca013af
hexin <hexin.work@outlook.com>
2018-05-01 20:50:31 +0800

    lab3

---
+++ lab3/Flik/TestFlik.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestFlik {
+
+    @Test
+    public void isSameNumber() {
+        assertTrue("128 should equal to 128", Flik.isSameNumber(128, 128));
+    }
+}
