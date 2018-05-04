cd92dfb144f787b8b6acf5e4b4415af747a19f2e
hexin <hexin.work@outlook.com>
2018-05-04 20:21:51 +0800

    Task 2: wordToDeque

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,9 @@
 public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> characterDeque = new LinkedListDeque<>();
+        for (Character c : word.toCharArray()) {
+            characterDeque.addLast(c);
+        }
+        return characterDeque;
+    }
 }
