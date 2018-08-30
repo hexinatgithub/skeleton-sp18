c6492089dcae37a19d463b52eaa82388acd8cc4a
hexin <hexin.work@outlook.com>
2018-08-30 18:56:04 +0800

    finish hw3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -12,6 +12,26 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        double lower = oomages.size() / 50.0;
+        double upper = oomages.size() / 2.5;
+        int[] fractions = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            fractions[i] = 0;
+        }
+
+        int bucketNum;
+        for (Oomage o : oomages) {
+            bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            fractions[bucketNum] += 1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            double f = fractions[i];
+            if (!(lower < f && f < upper)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
